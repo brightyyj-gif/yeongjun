d343dccd7a171e2509f6b4a8142eeff0eeabb04a
brightyyj <brightyyj@gmail.com>
2026-09-08 15:27:08 +0900

    a0908_1

---
+++ 0907/zishen/js/main.js
@@ -0,0 +1,50 @@
+$(function(){
+
+    $(window).resize(function(){ //브라우저 사이즈 변경시
+        var w = $(this).width(); // 브라우저 넓이를 감지
+        // console.log(w)
+        if(w <= 850) {
+
+        }else {
+            if($('.mobile_nav').hasClass('active') == true){
+                $('.mobile_nav').removeClass('active');
+                $('.transparency').removeClass('active');
+                $('.container').removeClass('active');
+                $('.mobile_nav .sub').css('display','none');
+            }
+        }
+    });
+
+    $('.nav ul').hover(function(){ // 두개인이유는 올렸을때 내렷을때
+        $(this).addClass('over');
+    },function(){
+        $(this).removeClass('over');
+    });
+
+    // 탭버튼 클릭시 모바일 메뉴 나오기
+    $('.mobile_tab').click(function(){
+        $('.mobile_nav').addClass('active');
+        $('.transparency').addClass('active');
+        $('.container').addClass('active');
+        return false;
+    });
+
+    // transparency 영역클릭시 모바일 메뉴 숨기기
+    $('.transparency').click(function(){
+        $('.mobile_nav').removeClass('active');
+        $('.transparency').removeClass('active');
+        $('.container').removeClass('active');
+        $('.mobile_nav .sub').css('display','none');
+    });
+    // 아코디언 메뉴
+    $('.mobile_nav > ul > li > a').click(function(){
+        var k = $(this).next('.sub').css('display');
+        // alert(k);
+        if(k === 'none'){
+            $('.mobile_nav .sub').slideUp(300);
+            $(this).next('.sub').slideDown(300);
+        }else {
+            $(this).next('.sub').slideUp(300);
+        }
+    });
+});
---
+++ 0908/pc-mobile-nav/js/main.js
@@ -0,0 +1,77 @@
+$(function(){
+
+
+// pc hover시
+$('nav> ul').hover(function(){
+  $(this).addClass("over");
+},function(){
+  $(this).removeClass("over");
+});
+
+//햄버거메뉴 클릭
+$(".mobile_tab").on('click',openMoblieNav);
+
+
+function openMoblieNav(){
+    $('.mobile_nav').addClass('active');
+    $(".transparency").addClass("active");
+    $(".mobile_tab").attr("aria-expanded", "true");
+    // 웹접근성만 필요 - 삭제무방
+    $("body").css("overflow", "hidden");
+}
+function closeMobileNav(){
+    $(".mobile_nav").removeClass("active");
+    $(".transparency").removeClass("active");
+    $(".mobile_tab").attr("aria-expanded", "false");
+    $(".mobile_nav .sub").slideUp(0);
+    $("body").css("overflow", "");
+    // 메뉴를 닫을때 overflow : hidden 계속 남아있어서
+    //버그가 생길때 
+}
+
+$('.transparency').on('click',closeMobileNav);
+$('.mobile_close').on('click',closeMobileNav);
+
+// 모바일 아코디언 서브
+
+ $(".mobile_nav > ul > li > a").on("click", function (e) {
+    var $sub = $(this).next(".sub");
+    if ($sub.length === 0) return;
+
+    e.preventDefault();
+    if ($sub.css("display") === "none") {
+      $(".mobile_nav .sub").slideUp(300);
+      $sub.slideDown(300);
+    } else {
+      $sub.slideUp(300);
+    }
+  });
+
+
+  var MOBILE_BP  = 850;
+
+//   리사이즈 : pc로 돌아가면 모바일메뉴 초기화
+$(window).on('resize',function(){
+    if($(window).width() >  MOBILE_BP){
+         closeMobileNav();
+    }
+});
+
+var $slider = $('.main-slider');
+
+$slider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 600,
+    fade: true,
+    cssEase: "linear",
+    arrows: false,
+    dots: true,
+    pauseOnHover: true,
+});
+
+
+});
